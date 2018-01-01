0923bdc84b3888f82f7defc7bdbd92badcd0bf62
Asif Khan <asifkhan.ak95.ak@gmail.com>
2018-01-01 14:24:15 +0530

    vcdvdv

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dotnet publish -o published
-sudo docker build -t asif121/testapplication:latest .
-sudo  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-sudo docker push asif121/testapplication:latest
+docker build -t asif121/testapplication:latest .
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker push asif121/testapplication:latest
